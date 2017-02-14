233c68c38986307c0f61b0fb93172e6875e139df
Davis <davisbri@tome11802.local>
2017-02-13 23:12:25 -0500

    Committed by davisbri @ 02/13/17 23:12:25

--- src/lab03/CountingPairs.java
+++ src/lab03/CountingPairs.java
@@ -3,6 +3,16 @@ package lab03;
 import java.util.*;
 
 /**
+ * Given N integers, count the number of pairs of those integers that have a
+ * difference equal to another integer K.
+ * 
+ * The first line of input will contain 2 space delimited integers giving the
+ * values of N and K with the following constraints:
+ * 
+ * 0 <= N <= 5000 0 <= K <= 2500
+ * 
+ * The second line of input will contain N space delimited integers. The value
+ * of each integer will be in the range [0...2500].
  * 
  * @author Dickinson College
  * @author Briona Davis, Seongho Lee
@@ -11,39 +21,42 @@ import java.util.*;
  */
 public class CountingPairs {
 	private int valN, valK; // value of K and N
-	private ArrayList<Long> nums;	// Number elements of the second line into an arraylist.
-	private HashMap<Long, Long> hashmap;
+	private ArrayList<Integer> nums; // contain the second line integers.
+	private HashMap<Integer, Integer> hashmap;
 
 	/**
-	 * Construct a new CountingPairs object and initialize an array of int with
-	 * random size.
+	 * Construct a new CountingPairs object and initialize an ArrayList of
+	 * Integer and create Hashmap of Integer key and Integer value.
 	 */
 	public CountingPairs() {
-		nums = new ArrayList<Long>();
-		hashmap = new HashMap<Long, Long>();
+		nums = new ArrayList<Integer>();
+		hashmap = new HashMap<Integer, Integer>();
 	}
 
 	/**
-	 *
+	 * The first line with integer values is read and stored into valN and valK,
+	 * respectively. The second line with integer values is read and stored in
+	 * an arraylist.
 	 */
 	public void readFile() {
 		Scanner scr = new Scanner(System.in);
-		//First the first line contains two integers, values of N and K, respectively..
+
 		valN = scr.nextInt();
 		valK = scr.nextInt();
 
-		//read off all elements in the file until it reaches the end.
+		// read off all elements in the file until it reaches the end.
 		for (int i = 0; i < valN; i++) {
-			nums.add((long) scr.nextInt());
+			nums.add((Integer) scr.nextInt());
 		}
 
 		scr.close();
 	}
 
 	/**
-	 * Does calculation + comparison + returns the output.
+	 * Find the number of pairs of integers with difference K.
 	 * 
-	 * @return
+	 * @return a single integer indicating the number of pairs of integers with
+	 *         difference K.
 	 */
 	public int countPairs() {
 		readFile();
@@ -51,43 +64,31 @@ public class CountingPairs {
 
 		for (int i = 0; i < valN; i++) {
 			if (hashmap.containsKey(nums.get(i))) {
+				// increase the frequency by 1 if it already contains the key.
 				hashmap.put(nums.get(i), hashmap.get(nums.get(i)) + 1);
 			} else {
-				hashmap.put(nums.get(i), (long) 1);
+				// add this new key into hashmap.
+				hashmap.put(nums.get(i), (Integer) 1);
 			}
 		}
 
-		Set<Long> key = hashmap.keySet();
-		for (Long x : key) {
+		Set<Integer> key = hashmap.keySet();
+		for (Integer x : key) {
 			if (valK == 0) {
+				// frequency of the number = n
+				// (n) * (n-1) / 2
 				count += hashmap.get(x) * (hashmap.get(x) - 1) / 2;
-			}
-			else if (x - valK >= 0 && hashmap.containsKey(x - valK)) {
+			} else if (x - valK >= 0 && hashmap.containsKey(x - valK)) {
+				// (frequency of the number) * (frequency of the other pair with the number)
 				count += hashmap.get(x) * hashmap.get(x - valK);
 			}
 		}
-		/*
-		 * Deleted 2/12/2017
-		for (int i = 0; i < valN; i++) {
-			int x = data[i];
-			if (valK == 0) {
-
-			}
-			if (x - valK >= 0 && hashmap.containsKey(x - valK)) {
-				count++;
-				hashmap.remove(x);
-			}
-			if (x + valK < MAX_K && hashmap.containsKey(x + valK)) {
-				count++;
-				hashmap.remove(x); // redundancy
-			}
-		}
-		*/
 
 		return count;
 	}
 
 	/**
+	 * Print the number of pairs of integers with difference K.
 	 * 
 	 * @param args
 	 *            none
--- src/lab03/CountingPairsTest.java
+++ src/lab03/CountingPairsTest.java
@@ -1,7 +1,6 @@
 package lab03;
 
-import java.io.File;
-
+import java.io.*;
 import org.junit.Test;
 
 import testbase.StdioTestBase;
@@ -13,42 +12,227 @@ public class CountingPairsTest extends StdioTestBase {
 		// Input from a string in the test.
 		String input = "5 2\n1 5 3 4 2";
 		Integer output = 3;
-		runTest(CountingPairs.class, input, output,
-				"Incorrect result for sample input 1.");
+		runTest(CountingPairs.class, input, output, "Incorrect result for sample input 1.");
 	}
-	
+
 	@Test
 	public void testSample2() {
 		// Input from a text file in the package.
 		File input = new File("src/lab03/input2.txt");
 		Integer output = 0;
-		runTest(CountingPairs.class, input, output,
-				"Incorrect result for sample input 2.");
+		runTest(CountingPairs.class, input, output, "Incorrect result for sample input 2.");
 	}
-	
+
+	/*
+	 * test if it returns the correct output when N = 2 and K = 0.
+	 */
 	@Test
-	public void testSamePositiveTwoNums() {
+	public void testSameTwoNumsWithDifference0() {
 		File input = new File("src/lab03/input3.txt");
-		Integer output = ;
-		runTest(CountingPairs.class, input, output,
-				"Incorrect result for sample input 3.");
+		Integer output = 1;
+		runTest(CountingPairs.class, input, output, "Incorrect result for input 3.");
 	}
-	
+
+	/*
+	 * test if it returns the correct output when K = 0 with the same multiple
+	 * elements.
+	 */
 	@Test
-	public void testSamePositiveMultipleNums() {
+	public void testSameMultipleNumsWithDifference0() {
 		File input = new File("src/lab03/input4.txt");
-		Integer output = ;
-		runTest(CountingPairs.class, input, output,
-				"Incorrect result for sample input 4.");
+		Integer output = 45;
+		runTest(CountingPairs.class, input, output, "Incorrect result for input 4.");
 	}
-	
-	
-	//N == 0, 2digits
-	//K == 0, some # in b/w, 2500 (2500, 0 elements)
-	//all same # in the 2nd line
-	//N == 5000
-	
-	//all zeros
-	//long # with 50000
-	
+
+	/*
+	 * test if it returns the correct output when it contains no elements on the
+	 * second line.
+	 */
+	@Test
+	public void testNequals0() {
+		File input = new File("src/lab03/input5.txt");
+		Integer output = 0;
+		runTest(CountingPairs.class, input, output, "Incorrect result for input 5.");
+	}
+
+	/*
+	 * test if it returns the correct output when it contains maximum number of
+	 * integers in the second line.
+	 */
+	@Test
+	public void testNequalsMax() {
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(new FileOutputStream("src/lab03/input6.txt", false));
+			pw.println("5000 1");
+			for (int i = 0; i < 2500; i++) {
+				pw.print("4 ");
+			}
+			for (int i = 2500; i < 5000; i++) {
+				pw.print("5 ");
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable to open file");
+		} finally {
+			pw.close();
+		}
+
+		File input = new File("src/lab03/input6.txt");
+		Integer output = 2500 * 2500;
+		runTest(CountingPairs.class, input, output, "Incorrect result for input 6.");
+	}
+
+	/*
+	 * test if it returns the correct output when it contains maximum number of
+	 * integers in the second line and the difference equals 0.
+	 */
+	@Test
+	public void testNequalsMaxWithKequalTo0() {
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(new FileOutputStream("src/lab03/input7.txt", false));
+			pw.println("5000 0");
+			for (int i = 0; i < 5000; i++) {
+				pw.print("0 ");
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable to open file");
+		} finally {
+			pw.close();
+		}
+
+		File input = new File("src/lab03/input7.txt");
+		Integer output = 5000 * 4999 / 2;
+		runTest(CountingPairs.class, input, output, "Incorrect result for input 7.");
+	}
+
+	/*
+	 * test if it returns the correct output when it contains large integers.
+	 * Note that even though the value of each integer will be in the range
+	 * [0...2500], this test will still pass because it's in the range of
+	 * integer.
+	 */
+	@Test
+	public void testLargeIntegers() {
+		PrintWriter pw = null;
+		int n = (int) (Math.random() * 10000000 + 2000000000);
+		try {
+			pw = new PrintWriter(new FileOutputStream("src/lab03/input8.txt", false));
+			pw.println("3745 1");
+			for (int i = 0; i < 3745; i++) {
+				pw.print(n + " ");
+				n++;
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable to open file");
+		} finally {
+			pw.close();
+		}
+
+		File input = new File("src/lab03/input8.txt");
+		Integer output = 3744;
+		runTest(CountingPairs.class, input, output, "Incorrect result for input 8.");
+	}
+
+	/*
+	 * test if it returns the correct output when it contains few elements and K
+	 * is max. The difference of the integers will not equal 0, which means that
+	 * output should be 0.
+	 */
+	@Test
+	public void testMaxKWithFewElementsOutputEquals0() {
+		File input = new File("src/lab03/input9.txt");
+		Integer output = 0;
+		runTest(CountingPairs.class, input, output, "Incorrect result for input 9.");
+	}
+
+	/*
+	 * test if it returns the correct output when it contains few elements and K
+	 * is max.
+	 */
+	@Test
+	public void testMaxKWithFewElements() {
+		File input = new File("src/lab03/input10.txt");
+		Integer output = 2;
+		runTest(CountingPairs.class, input, output, "Incorrect result for input 10.");
+	}
+
+	/*
+	 * test if it returns the correct output when it contains few 0s and 2500s
+	 * and K equals 2500.
+	 */
+	@Test
+	public void testMaxK() {
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(new FileOutputStream("src/lab03/input11.txt", false));
+			pw.println("2340 2500");
+			pw.print("0 0 1 0 2 3 0 0 0 1030 ");
+			for (int i = 0; i < 2320; i++) {
+				int n = (int) (Math.random() * 2400 + 1);
+				pw.print(n + " ");
+			}
+			pw.print("302 1934 2500 2500 1293 2500 2030 440 2500 123 444");
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable to open file");
+		} finally {
+			pw.close();
+		}
+
+		File input = new File("src/lab03/input11.txt");
+		Integer output = 24;
+		runTest(CountingPairs.class, input, output, "Incorrect result for input 11.");
+	}
+
+	/*
+	 * test if it returns the correct output when it contains all same numbers
+	 * but k is not 0.
+	 */
+	@Test
+	public void testAllSameNums() {
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(new FileOutputStream("src/lab03/input12.txt", false));
+			pw.println("2300 1234");
+			for (int i = 0; i < 2300; i++) {
+				pw.print("3 ");
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable to open file");
+		} finally {
+			pw.close();
+		}
+
+		File input = new File("src/lab03/input12.txt");
+		Integer output = 0;
+		runTest(CountingPairs.class, input, output, "Incorrect result for input 12.");
+	}
+
+	/*
+	 * test if it returns the correct output when the values of N and K are at
+	 * the max and the values of each integer are made up of 0s and 2500s.
+	 */
+	@Test
+	public void testMaxForBothNandK() {
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(new FileOutputStream("src/lab03/input13.txt", false));
+			pw.println("5000 2500");
+			for (int i = 0; i < 2500; i++) {
+				pw.print("0 ");
+			}
+			for (int i = 2500; i < 5000; i++) {
+				pw.print("2500 ");
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Unable to open file");
+		} finally {
+			pw.close();
+		}
+
+		File input = new File("src/lab03/input13.txt");
+		Integer output = 2500 * 2500;
+		runTest(CountingPairs.class, input, output, "Incorrect result for input 13.");
+	}
+
 }
