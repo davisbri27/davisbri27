e1ca7f4daa55837ed21ec9f631865b54fdad3d49
skalakm <skalakm@dickinson.edu>
2017-03-31 14:39:32 -0400

    graded lab7

--- src/lab07/FerryBoat.java
+++ src/lab07/FerryBoat.java
@@ -1,127 +1,179 @@
 package lab07;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
+import lab06.ParadeShuffle;
 
-public class FerryBoat {
-	private Queue<Car> leftQueue;
-	private Queue<Car> rightQueue;
-	private int ferryLen;
-	private int numCars;
-	private int numTrips;
+/**
+ * Using rightSide and leftSide to find how many trips it takes to transport all the cars on each side of the bay. 
+ * The ferry must always start from the left side and end up in left side at the end of the day.
+ * @author JuHyung Jeong and Briona Davis
+ * @version March 23, 2017
+ *
+ */
+public class FerryBoat{
 
+	private int lengthFerry; // length of the ferry
+	private int numCars; // number of cars waiting
+	private Queue<car> rightSide; // queue for the cars on the right side
+	private Queue<car> leftSide; // queue for the cars on the left side
+	private int numTrips; // number of trips
 
-	public void readFile(){
-		leftQueue= new LinkedList<Car>();
-		rightQueue= new LinkedList<Car>();
+	/**
+	 *Private class to make car objects to add to the Queues
+	 */
+	private class car {
 
-		Scanner scr= new Scanner(System.in);
-		ferryLen= scr.nextInt()*100;
-		numCars= scr.nextInt();
-
-		while(scr.hasNext()){
-			Car curCar= new Car(scr.nextInt(), scr.next(), scr.next());
-
-			if( curCar.getSide().equals("left")){
-				leftQueue.add(curCar);
-
-			}else if(curCar.getSide().equals("right")){
-				rightQueue.add(curCar);
-
-			}
-
-		}
-		scr.close();
-
-	}
-
-	public void ferryRide(){
-
-
-		if(numCars==0){
-			System.out.println("Day Off!");
-
-		}else{
-
-
-			int space=0;
-			numTrips=0;
-			
-			while(!leftQueue.isEmpty()&& !rightQueue.isEmpty()){
-				space=0;
-				numTrips++;
-				System.out.println(numTrips+ " : ");
-
-				while(!leftQueue.isEmpty() && leftQueue.peek().getLength()+space <=ferryLen){
-					space=space+ leftQueue.peek().getLength();
-					System.out.print(leftQueue.remove().getPlate());	
-				}
-				numTrips++;
-				System.out.println(numTrips+ " : ");
-				space=0;
-
-				while(!rightQueue.isEmpty() &&rightQueue.peek().getLength()+space <=ferryLen){
-					space=space+ rightQueue.peek().getLength();
-					System.out.print(rightQueue.remove().getPlate());	
-				}
-				
-			}
-
-			if(numTrips %2 !=0){ //if the ferry trip is on the right side
-				numTrips++;
-				System.out.println(numTrips+ " : empty");
-
-
-
-			}
-		}
-
-
-
-
-
-
-
-	}
-
-
-
-
-
-
-	public class Car{
 		private int length;
 		private String plate;
 		private String side;
-
-		public Car(int length, String plate, String side){
-			this.length=length;
-			this.plate=plate;
-			this.side=side;
+		
+		/**
+		 * constructor that will create car objects
+		 * @param length length of the car
+		 * @param plate the plate 
+		 * @param side the side where the car is in
+		 */
+		private car(int length, String plate, String side){
+			this.length = length;
+			this.plate = plate;
+			this.side = side;
 		}
-
-		public int getLength(){
+		
+		/**
+		 * gets the length of the car
+		 * @return length of the car
+		 */
+		private int getCarLength(){
 			return length;
 		}
-
-		public String getPlate(){
+		
+		/**
+		 * gets the plate of the car
+		 * @return plate of the car
+		 */
+		private String getCarPlate(){
 			return plate;
 		}
-
-		public String getSide(){
+		
+		/**
+		 * gets which side the car is on
+		 * @return side of the car
+		 */
+		private String getCarSide(){
 			return side;
 		}
 
-
+	}
+	
+	/**
+	 * Constructor for the FerryBoat class and sets up the leftSide queue and rightSide queue to use
+	 */
+	public FerryBoat() {
+		leftSide = new LinkedList<car>();
+		rightSide = new LinkedList<car>();
 	}
 
+	/**
+	 * reads the input and sets up all the variables that will be used later
+	 */
+	public void init(){
+		Scanner in = new Scanner(System.in);
+		lengthFerry = in.nextInt() * 100;
+		numCars = in.nextInt();
+		numTrips = 1;
+		while(in.hasNext()){
+			car curCar = new car(in.nextInt(), in.next(), in.next());
+			if(curCar.getCarSide().equals("left")){
+				leftSide.add(curCar);
+			} else {
+				rightSide.add(curCar);
+			}
+		}
+	}
 
+	/**
+	 * Prints out the number of trips and cars that were transported to the other side
+	 */
+	public void ferryRide(){
+		if(numCars == 0){
+			// if there are no cars on both sides of the bay, then the captain gets a day off
+			System.out.print("Day Off!");
+		} else {
+			while (!leftSide.isEmpty() || !rightSide.isEmpty()){
+				if (numTrips % 2 != 0){
+					System.out.print(numTrips + " :");
+					leftToRight();
+					System.out.println();
+					numTrips++;
+				} else {
+					System.out.print(numTrips + " :");
+					rightToLeft();
+					System.out.println();
+					numTrips++;
+				}
+			}
+		}
+		//if the ferry trip ends on the right side and there are no more cars on the both sides
+		if(numTrips % 2 == 0){ 
+			System.out.print(numTrips + " :"+ " empty");
+		}
+	}
+	
+	/**
+	 * Helper method to transfer the cars from the left side to the right side
+	 */
+	private void leftToRight(){
+		int space = 0;
+		int carNumber = 0; // the number of cars that are getting transported during 1 trip
+		while (!leftSide.isEmpty()){
+			//checks if the car can fit into the ferry
+			if(leftSide.peek().getCarLength() + space <= lengthFerry){
+				space += leftSide.peek().getCarLength();
+				// prints out the plate number
+				System.out.print(" " + leftSide.peek().getCarPlate());
+				leftSide.remove();
+				carNumber++;
+			} else {
+				break;
+			}
+		}
+		if(carNumber == 0){
+			//if no cars were transported 
+			System.out.print(" empty");
+		}
+	}
 
-
-
+	/**
+	 * Helper method to transfer the cars from the left side to the right side
+	 */
+	private void rightToLeft(){
+		int space = 0;
+		int carNumber = 0; // the number of cars that are getting transported during 1 trip
+		while(!rightSide.isEmpty()){
+			//checks if the car can fit into the ferry
+			if(rightSide.peek().getCarLength() + space <= lengthFerry){
+				space += rightSide.peek().getCarLength();
+				//prints out the plate number
+				System.out.print(" " + rightSide.peek().getCarPlate());
+				rightSide.remove();
+				carNumber++;
+			} else {
+				break;
+			}
+		}
+		if(carNumber == 0){
+			//if no cars were transported
+			System.out.print(" empty");
+		}
+	}
 
 	public static void main(String[] args) {
-
+		FerryBoat p = new FerryBoat();
+		p.init(); // initializes the values to use
+		p.ferryRide(); // prints out the results of each ferry rides
 	}
 }
