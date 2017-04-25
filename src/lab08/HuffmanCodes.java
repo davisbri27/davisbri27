45031f699cca59c783930160516d4597053c0eff
skalakm <skalakm@dickinson.edu>
2017-04-25 14:50:50 -0400

    graded lab8

--- src/lab08/HuffmanCodes.java
+++ src/lab08/HuffmanCodes.java
@@ -1,9 +1,254 @@
 package lab08;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+/**
+ * Reads the input and prints out the different answers according to the input desires.
+ * @author jeongj
+ * @version April 11, 2017
+ */
+
 public class HuffmanCodes {
 
-	public static void main(String[] args) {
+	private static String firstLetter; // Letter that tells which method should be ran
+	private String inputString; // String of all the inputs
+	private String tempString; // holds the line of input as a String 
+	private HashMap<Character, Integer> numberHash; // holds the key and frequency of the input
+	private static CS232ArrayHeap<Node, CS232LinkedBinaryTree<Integer, Character>> nodeQueue; // holds the "trees" 
+	private String[] storeArr; // stores all the string value for method letter H and M (e.g 001, 0, 100)
 
+	/**
+	 *creates the "Node" that will be in the nodeQueue that will ultimately be combined to create tree
+	 */
+	public class Node implements Comparable<Node> {
+		private int frequency;
+		private char key;
+
+		//constructor to create the Node
+		public Node(Integer frequency, Character key) {
+			this.frequency = frequency;
+			this.key = key;
+		}
+		//returns frequency
+		public int getFrequency() {
+			return frequency;
+		}
+		
+		//returns key
+		public char getKey() {
+			return key;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			// TODO Auto-generated method stub
+			if (frequency > o.frequency) {
+				return -1;
+			}
+			//if the frequency is same, then compare the key values
+			else if (frequency == o.frequency) {
+				if (key > o.key) {
+					return -1;
+				} else if (key == o.key) {
+					return 0;
+				} else {
+					return 1;
+				}
+			} else {
+				return 1;
+			}
+		}
+	}
 	
+	/**
+	 * implement a visitor to print the tree in level order(character and frequency).
+	 */
+	private class PrintVisitor implements CS232Visitor<Integer, Character> {
+
+		/**
+		 * Print out the key,value pair at each node visited.
+		 * 
+		 * @param key
+		 *            the key for the current node.
+		 * @param value
+		 *            the value at the current node.
+		 */
+
+		@Override
+		public void visit(Integer key, Character value) {
+			// speical case when there is a tab
+			if (value.equals('\t')) {
+				System.out.println("\\t" + ":" + key);
+			}// special case when there is a new line 
+			else if (value.equals('\n')) {
+				System.out.println("\\n" + ":" + key);
+			}// if no special case just print out value and key 
+			else {
+				System.out.println(value + ":" + key);
+			}
+		}
+
+	}
+	
+	// constructor of the huffmanCodes, initializes the fields that we need
+	public HuffmanCodes() {
+		numberHash = new HashMap<Character, Integer>();
+		nodeQueue = new CS232ArrayHeap<Node, CS232LinkedBinaryTree<Integer, Character>>();
+		storeArr = new String[128];
+	}
+
+	//reads all the input
+	public void inputReader() {
+		Scanner in = new Scanner(System.in);
+		firstLetter = in.next(); // finds out which method to run
+		inputString = "";
+		in.nextLine();
+		while (in.hasNextLine()) {
+			tempString = in.nextLine(); // puts the first line of the input as the tempString
+			if (in.hasNextLine()) { // if there is an another line after tempString then add \n
+				tempString += "\n";
+			}
+			for (int i = 0; i < tempString.length(); i++) {
+				//reads each char of the tempString and add to the hashmap
+				char curChar = tempString.charAt(i);
+				Integer val = numberHash.get(new Character(curChar));
+				//val+1 when there is already existing key
+				if (numberHash.get(new Character(curChar)) != null) {
+					numberHash.put(curChar, new Integer(val + 1));
+				}// put into the hashmap if there is no existing key 
+				else {
+					numberHash.put(curChar, 1);
+				}
+			}
+			//adds all the tempString to the inputString, this saves the whole input in one string
+			inputString += tempString;
+		}
+		in.close();
+	}
+	
+	//prints out the frequency table
+	public void frequencyTable() {
+		Map<Character, Integer> map = numberHash;
+		// since the hashmap isn't sorted by the ASCII order, it needs to be sorted
+		Object[] keys = map.keySet().toArray(); // puts everything into Array
+		Arrays.sort(keys); // sorts key sets in ASCII order in the array
+
+		//prints out in ASCII order
+		for (int i = 0; i < keys.length; i++) {
+			if (keys[i].equals('\t')) {
+				//special case when there is a tab
+				System.out.println("\\t" + ":" + map.get((char)keys[i]));
+			} else if (keys[i].equals('\n')) {
+				//special case when there is a new line
+				System.out.println("\\n" + ":" + map.get((char)keys[i]));
+			} else {
+				System.out.println((char)keys[i] + ":" + map.get((char)keys[i]));
+			}
+		}
+	}
+	
+	//adds all the "nodes" or "trees" to the NodeQueue, priority queue, from the hashmap. 
+	public void addSingleNode() {
+
+		for (Map.Entry<Character, Integer> entry : numberHash.entrySet()) {
+			CS232LinkedBinaryTree<Integer, Character> singleNode = new CS232LinkedBinaryTree<Integer, Character>(
+					entry.getValue(), entry.getKey());
+			Node curNode = new Node(entry.getValue(), entry.getKey());
+			nodeQueue.add(curNode, singleNode);
+		}
+	}
+
+	//puts everything into the nodeQueue after combining the "nodes" or "trees" that are in the queue. 
+	public void treeNode() {
+		while (nodeQueue.size() > 1) {
+			CS232LinkedBinaryTree<Integer, Character> tree1 = nodeQueue.remove();
+			CS232LinkedBinaryTree<Integer, Character> tree2 = nodeQueue.remove();
+			//reads which one has the higher frequency and decides which key will represent the new "node" or "tree"
+			if (tree1.getRootValue() > tree2.getRootValue()) {
+				CS232LinkedBinaryTree<Integer, Character> newTree = new CS232LinkedBinaryTree<Integer, Character>(tree2,
+						tree1.getRootKey() + tree2.getRootKey(), tree2.getRootValue(), tree1);
+				Node curNode = new Node(newTree.getRootKey(), newTree.getRootValue());
+				nodeQueue.add(curNode, newTree);
+
+			} else {
+				CS232LinkedBinaryTree<Integer, Character> newTree = new CS232LinkedBinaryTree<Integer, Character>(tree2,
+						tree1.getRootKey() + tree2.getRootKey(), tree1.getRootValue(), tree1);
+				Node curNode = new Node(newTree.getRootKey(), newTree.getRootValue());
+				nodeQueue.add(curNode, newTree);
+			}
+		}
+	}
+	
+	//prints level order of the tree that was created by combining individual "node" or "tree"
+	public void printLevelOrder() {
+		String curString = null;
+		while (nodeQueue.size() > 0) {
+				CS232LinkedBinaryTree<Integer, Character> bt = nodeQueue.remove();
+				bt.visitLevelOrder(new PrintVisitor());
+		}
+	}
+	
+	// finds the huffman code (e.g 100, 001, 01) for each leaves. 
+	public void findHuffmanCode() {
+		CS232LinkedBinaryTree<Integer, Character> tree = nodeQueue.peek();
+		findHuffmanCodeRecHelper(tree.root, "", storeArr);
+	}
+	
+	//recursion helper that will find the huffman code. 
+	public void findHuffmanCodeRecHelper(CS232LinkedBinaryTree.BTNode<Integer, Character> node, String str, String[] storeArr) {
+		if (node.isLeaf()) {
+			storeArr[node.value] = str;
+		} else {
+			if (node.right != null) {
+				// if the leaf is to the right then prints 1
+				findHuffmanCodeRecHelper(node.right, str + "1", storeArr);
+			}
+			if (node.left != null) {
+				// if the leaf is to the left then prints 0
+				findHuffmanCodeRecHelper(node.left, str + "0", storeArr);
+			}
+		}
+	}
+
+	//prints the huffman code. It reads where the leaf is and prints 1 if the leaf is to the right and 0 otherwise. 
+	public void printHuffmanCode() {
+		for (int i = 0; i < 128; i++) {
+			if (storeArr[i] != null) {
+				if((char)i == '\n'){
+					System.out.println("\\n:" + storeArr[i]);
+				} else if ((char)i == '\t'){
+					System.out.println("\\t:" + storeArr[i]);
+				} else {
+				System.out.println((char) i + ":" + storeArr[i]);
+				}
+			}
+		}
+	}
+
+	// gets the huffman code for each character and prints the huffman code for each characters
+	public void printHuffmanCodeForEachCharacter() {
+		findHuffmanCode();
+		for (int i = 0; i < inputString.length(); i++) {
+			System.out.print(storeArr[inputString.charAt(i)]);
+		}
+	}
+
+	public static void main(String[] args) {
+		HuffmanCodes huffMan = new HuffmanCodes(); // initializes the huffman
+		huffMan.inputReader(); // reads all the input
+		huffMan.addSingleNode(); //adds all the single "nodes" or "trees" to the priority queue
+		huffMan.treeNode(); // combines every "node" or "tree" into one big binary tree
+		if (firstLetter.equals("F")) { //if the first letter is F
+			huffMan.frequencyTable();
+		} else if (firstLetter.equals("T")) { // if the first letter is T
+			huffMan.printLevelOrder();
+		} else if (firstLetter.equals("H")) { // if the first letter is H
+			huffMan.findHuffmanCode();
+			huffMan.printHuffmanCode();
+		} else if (firstLetter.equalsIgnoreCase("M")) { // if the first letter is M
+			huffMan.printHuffmanCodeForEachCharacter();
+		}
 	}
 }
--- src/lab08/HuffmanCodesTest.java
+++ src/lab08/HuffmanCodesTest.java
@@ -1,11 +1,22 @@
 package lab08;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.PriorityQueue;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import testbase.StdioTestBase;
 
 public class HuffmanCodesTest extends StdioTestBase {
-		
+
+	@Before
+	public void setUp() {
+		HuffmanCodes huffMan = new HuffmanCodes();
+	}
+
+
 	@Test
 	public void testSampleInput1() {
 		String input = "F\nAABBBCDDDDEE";
@@ -14,7 +25,7 @@ public class HuffmanCodesTest extends StdioTestBase {
 		runTest(HuffmanCodes.class, input, output,
 				"Incorrect result for sample input 1.");
 	}
-	
+
 	@Test
 	public void testSampleInput2() {
 		String input = "T\nAABBBCDDDDEE";
@@ -23,7 +34,7 @@ public class HuffmanCodesTest extends StdioTestBase {
 		runTest(HuffmanCodes.class, input, output,
 				"Incorrect result for sample input 2.");
 	}
-	
+
 	@Test
 	public void testSampleInput3() {
 		String input = "H\nAABBBCDDDDEE";
@@ -32,7 +43,7 @@ public class HuffmanCodesTest extends StdioTestBase {
 		runTest(HuffmanCodes.class, input, output,
 				"Incorrect result for sample input 3.");
 	}
-	
+
 	@Test
 	public void testSampleInput4() {
 		String input = "M\nAABBBCDDDDEE";
@@ -41,4 +52,169 @@ public class HuffmanCodesTest extends StdioTestBase {
 		runTest(HuffmanCodes.class, input, output,
 				"Incorrect result for sample input 4.");
 	}
+
+	@Test
+	public void testFrequencyTab(){
+		String input = "F\nAA\tBBB\tCDDDDEE";
+		String output = "\\t:2\nA:2\nB:3\nC:1\nD:4\nE:2";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of tabs in the output");
+	}
+
+	@Test
+	public void testFrequencyNewLine(){
+		String input = "F\n\nAA\nBBB\nCDDDDEE";
+		String output = "\\n:3\nA:2\nB:3\nC:1\nD:4\nE:2";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of new lines in the output");
+	}
+
+	@Test
+	public void testFrequencySpace(){
+		String input = "F\nAA BBB CDD DD EE";
+		String output = " :4\nA:2\nB:3\nC:1\nD:4\nE:2";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of spaces in the output");
+	}
+
+	@Test
+	public void testTreeNodeTab() {
+		String input = "T\nA\tAAAB";
+		String output = "\\t:6\nA:4\n\\t:2\nB:1\n\\t:1";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of tabs in the treeNode");
+	}
+
+	@Test
+	public void testTreeNodeTab2() {
+		String input = "T\n\nA\tAB\tBBCDDDDEE";
+		String output = "\\t:15\nA:8\n\\t:7\nD:4\nA:4\n\\t:4\nB:3\nE:2\nA:2\n\\n:2\n\\t:2\nC:1\n\\n:1";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of tabs in the output for treeNode");
+	}
+
+	@Test
+	public void testTreeNodeNewLine(){
+		String input = "T\n\nAA\nBBB\nCDDDDEE";
+		String output = "\\n:15\n\\n:9\nA:6\n\\n:5\nD:4\nB:3\nA:3\n\\n:3\nE:2\nA:2\nC:1";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of new lines in the output for treeNode");
+	}
+
+	@Test
+	public void testTreeNodeSpace(){
+		String input = "T\nAA BBB CDD DD EE";
+		String output = " :16\nA:9\n :7\nA:5\nD:4\n :4\nB:3\nA:3\nE:2\nA:2\nC:1";
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of spaces in the output for treeNode");	
+	}
+
+	@Test
+	public void testHuffmanCodeTab(){
+		String input = "H\n\tAAA\tB";
+		String output = "\\t:10\nA:0\nB:11";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of tabs in the huffmanCode output");
+	}
+
+	@Test
+	public void testHuffmanCodeNewLine(){
+		String input = "H\n\nA\nBBB\nE";
+		String output = "\\n:00\nA:011\nB:1\nE:010";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of new lines in the huffmanCode output");
+	}
+
+	@Test
+	public void testHuffmanCodeSpace(){
+		String input = "H\nAA BBB C";
+		String output = " :000\nA:01\nB:1\nC:001";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of spaces in the huffmanCode output");
+	}
+
+	@Test
+	public void testHuffmanTextTab(){
+		String input = "M\n\tAAA\tB";
+		String output = "100001011";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of tabs in the huffmanCode output");
+	}
+
+	@Test
+	public void testHuffmanTextNewLine(){
+		String input = "M\n\nA\nBBB\nE";
+		String output = "000110011100010";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of new lines in the huffmanCode output");
+	}
+
+	@Test
+	public void testHuffmanTextSpace(){
+		String input = "M\nAA BBB C";
+		String output = "0101000111000001";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect number of spaces in the huffmanCode output");
+	}
+
+	@Test
+	public void testTTreeOnlyRoot(){
+		String input = "T\nAAA";
+		String output = "A:3";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect in the treeNode output when there is only one root");
+	}
+
+	@Test
+	public void testTreeNodeAllKeyEqual(){
+		String input = "T\nABCDDCBA";
+		String output = "A:8\nC:4\nA:4\nD:2\nC:2\nB:2\nA:2";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect in the treeNode same key output");
+
+	}
+	
+	@Test
+	public void testHuffmanCodeAllKeyEqual(){
+		String input = "H\nABCDDCBA";
+		String output = "A:11\nB:10\nC:01\nD:00";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect in the HuffmanCode same key output");
+	}
+	
+	@Test
+	public void testHuffmanTextKeyEqual(){
+		String input = "M\nABCDDCBA";
+		String output = "1110010000011011";
+
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect in the HuffmanText same key output");
+	}
+
+	@Test
+	public void testHuffmanCodeOneInput(){
+		String input = "M\nm";
+		String output = "";
+		
+		runTest(HuffmanCodes.class, input, output,
+				"Incorrect output in the HuffmanText one input test");
+	}
+
+
 }
+
